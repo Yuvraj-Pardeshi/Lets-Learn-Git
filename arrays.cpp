fb34736858e31f8fbf479749dd0fa3d183750f2f
Yuvraj Pardeshi <yuvrajpardeshi75@gmail.com>
2022-08-28 14:36:27 +0530

    added message in arrays.pp

--- arrays.cpp
+++ arrays.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    //printing an array
+    //printing an array             
     int arr[] = {1,2,3,4,5};
     int n = 5;
     for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+        cout<<"arr->"<<i<<" "<<arr[i]<<endl;
     }cout<<endl;
     return 0;
 }
